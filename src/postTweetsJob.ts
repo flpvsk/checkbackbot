b03ea66b944adc2706dce66aeec8af7ec6023f60
Andrey Salomatin <filipovskii.off@gmail.com>
2021-01-25 12:09:55 +0100

    Lint fix

--- src/postTweetsJob.ts
+++ src/postTweetsJob.ts
@@ -1,102 +1,93 @@
-import { OutgoingTweetType, TweetId, UserId } from "./types"
-import { storage } from "./storage"
-import * as twitter from "./twitterWrapper"
-import {formatDistance} from "date-fns"
-const JOB_NAME = "postTweets"
-
-interface ScrapeMentionsJobData {
-  mostRecentTweetId?: TweetId
-  error?: string
-}
+import { OutgoingTweetType, TweetId, UserId } from './types';
+import { storage } from './storage';
+import * as twitter from './twitterWrapper';
+import { formatDistance } from 'date-fns';
+export const JOB_NAME = 'postTweets';
 
 export async function run(): Promise<void> {
-  const accessKey = process.env.TW_ACCESS_TOKEN
-  const accessSecret = process.env.TW_ACCESS_TOKEN_SECRET
-  const consumerKey  = process.env.TW_API_KEY
-  const consumerSecret = process.env.TW_API_SECRET_KEY
+    const accessKey = process.env.TW_ACCESS_TOKEN;
+    const accessSecret = process.env.TW_ACCESS_TOKEN_SECRET;
+    const consumerKey = process.env.TW_API_KEY;
+    const consumerSecret = process.env.TW_API_SECRET_KEY;
 
-  if (!consumerKey || !consumerSecret || !accessKey || !accessSecret) {
-    console.warn("credentials missing, aborting")
-    return
-  }
-  const twitterV1Auth = {
-    consumerKey,
-    consumerSecret,
-    accessKey,
-    accessSecret
-  }
+    if (!consumerKey || !consumerSecret || !accessKey || !accessSecret) {
+        console.warn('credentials missing, aborting');
+        return;
+    }
+    const twitterV1Auth = {
+        consumerKey,
+        consumerSecret,
+        accessKey,
+        accessSecret,
+    };
 
-  const token = process.env.TW_BEARER_TOKEN
+    const token = process.env.TW_BEARER_TOKEN;
 
-  if (!token) {
-    console.warn("TW_BEARER_TOKEN not set, aborting")
-    return
-  }
-
-  const data = await storage.listPendingTweets({ toPostBefore: new Date() })
-  const userIds: Set<UserId> = new Set()
-  const userIdsByTweetId: Map<TweetId, UserId[]> = new Map()
-  for (const entry of data) {
-    const tweetUsers = Array.from(new Set([
-      entry.tweetData.inReplyToAuthor.id,
-      entry.tweetData.tweetAuthor.id
-    ]))
-
-    for (let uId of tweetUsers) {
-      userIds.add(uId)
+    if (!token) {
+        console.warn('TW_BEARER_TOKEN not set, aborting');
+        return;
     }
 
-    userIdsByTweetId.set(entry.tweetData.tweet.id, [ entry.tweetData.tweetAuthor.id ])
-  }
+    const data = await storage.listPendingTweets({ toPostBefore: new Date() });
+    const userIds: Set<UserId> = new Set();
+    const userIdsByTweetId: Map<TweetId, UserId[]> = new Map();
+    for (const entry of data) {
+        const tweetUsers = Array.from(new Set([entry.tweetData.inReplyToAuthor.id, entry.tweetData.tweetAuthor.id]));
 
-  const usersMap = await twitter.getUsersByIds({
-    userIds: Array.from(userIds),
-    token,
-  })
+        for (const uId of tweetUsers) {
+            userIds.add(uId);
+        }
 
-  for (const entry of data) {
-    const quoteTweetId = entry.tweetData.inReplyTo.id
-    const quoteTweetUsername = usersMap.get(
-      entry.tweetData.inReplyToAuthor.id
-    )?.username ?? "twitter"
-
-    if (entry.tweetType === OutgoingTweetType.repost) {
-      const mentionUsernames = userIdsByTweetId
-        .get(entry.tweetData.tweet.id)
-        ?.map(uId => usersMap.get(uId)?.username ?? "") ?? []
-
-      if (!mentionUsernames) continue
-
-      const distance = formatDistance(
-        Date.parse(entry.tweetData.inReplyTo.created_at),
-        new Date()
-      )
-
-      const text = `following up on this tweet ${distance} after it was posted`
-
-      const result = await twitter.quoteTweet({
-        mentionUsernames,
-        text,
-        quoteTweetId,
-        quoteTweetUsername
-      }, twitterV1Auth)
-
-      await storage.markPendingTweetPosted(entry.id, result)
+        userIdsByTweetId.set(entry.tweetData.tweet.id, [entry.tweetData.tweetAuthor.id]);
     }
 
-    if (entry.tweetType === OutgoingTweetType.confirmation) {
-      const text = `ok will follow up on the original tweet on ${entry.metaData.toPostOn}`
+    const usersMap = await twitter.getUsersByIds({
+        userIds: Array.from(userIds),
+        token,
+    });
 
-      const result = await twitter.replyToTweet({
-        replyToTweetId: entry.tweetData.tweet.id,
-        replyToUsername: usersMap.get(entry.tweetData.tweet.author_id)?.username ?? "",
-        text,
-        quoteTweetId,
-        quoteTweetUsername
-      }, twitterV1Auth)
+    for (const entry of data) {
+        const quoteTweetId = entry.tweetData.inReplyTo.id;
+        const quoteTweetUsername = usersMap.get(entry.tweetData.inReplyToAuthor.id)?.username ?? 'twitter';
 
-      await storage.markPendingTweetPosted(entry.id, result)
+        if (entry.tweetType === OutgoingTweetType.repost) {
+            const mentionUsernames =
+                userIdsByTweetId.get(entry.tweetData.tweet.id)?.map((uId) => usersMap.get(uId)?.username ?? '') ?? [];
+
+            if (!mentionUsernames) continue;
+
+            const distance = formatDistance(Date.parse(entry.tweetData.inReplyTo.created_at), new Date());
+
+            const text = `following up on this tweet ${distance} after it was posted`;
+
+            const result = await twitter.quoteTweet(
+                {
+                    mentionUsernames,
+                    text,
+                    quoteTweetId,
+                    quoteTweetUsername,
+                },
+                twitterV1Auth,
+            );
+
+            await storage.markPendingTweetPosted(entry.id, result);
+        }
+
+        if (entry.tweetType === OutgoingTweetType.confirmation) {
+            const text = `ok will follow up on the original tweet on ${entry.metaData.toPostOn}`;
+
+            const result = await twitter.replyToTweet(
+                {
+                    replyToTweetId: entry.tweetData.tweet.id,
+                    replyToUsername: usersMap.get(entry.tweetData.tweet.author_id)?.username ?? '',
+                    text,
+                    quoteTweetId,
+                    quoteTweetUsername,
+                },
+                twitterV1Auth,
+            );
+
+            await storage.markPendingTweetPosted(entry.id, result);
+        }
     }
-  }
 }
-
--- src/scrapeMentionsJob.ts
+++ src/scrapeMentionsJob.ts
@@ -1,73 +1,71 @@
-import * as chrono from "chrono-node"
-import { TweetId } from "./types"
-import { storage } from "./storage"
-import * as twitter from "./twitterWrapper"
-const JOB_NAME = "scrapeMentions"
+import * as chrono from 'chrono-node';
+import { TweetId } from './types';
+import { storage } from './storage';
+import * as twitter from './twitterWrapper';
+export const JOB_NAME = 'scrapeMentions';
 
 interface ScrapeMentionsJobData {
-  mostRecentTweetId?: TweetId
-  error?: string
+    mostRecentTweetId?: TweetId;
+    error?: string;
 }
 
 export async function run(): Promise<void> {
-  const lastRun = await storage.getLastJobData<ScrapeMentionsJobData>(JOB_NAME)
+    const lastRun = await storage.getLastJobData<ScrapeMentionsJobData>(JOB_NAME);
 
-  const userId = process.env.TW_BOT_USER_ID
-  const token = process.env.TW_BEARER_TOKEN
+    const userId = process.env.TW_BOT_USER_ID;
+    const token = process.env.TW_BEARER_TOKEN;
 
-  if (!userId) {
-    console.warn("TW_BOT_USER_ID not set, aborting")
-    return
-  }
+    if (!userId) {
+        console.warn('TW_BOT_USER_ID not set, aborting');
+        return;
+    }
 
-  if (!token) {
-    console.warn("TW_BEARER_TOKEN not set, aborting")
-    return
-  }
+    if (!token) {
+        console.warn('TW_BEARER_TOKEN not set, aborting');
+        return;
+    }
 
-  const tweets = twitter.getMentions({
-    after: lastRun?.mostRecentTweetId,
-    userId,
-    token,
-  })
+    const tweets = twitter.getMentions({
+        after: lastRun?.mostRecentTweetId,
+        userId,
+        token,
+    });
 
-  let mostRecentTweetId: TweetId | undefined;
-  let error: string | undefined;
-  try {
-    for await (const tweetInfo of tweets) {
-      if (!mostRecentTweetId) {
-        mostRecentTweetId = tweetInfo.tweet.id
-      }
+    let mostRecentTweetId: TweetId | undefined;
+    let error: string | undefined;
+    try {
+        for await (const tweetInfo of tweets) {
+            if (!mostRecentTweetId) {
+                mostRecentTweetId = tweetInfo.tweet.id;
+            }
 
-      // it's a tweet for this bot
-      /*
+            // it's a tweet for this bot
+            /*
       if (tweetInfo.tweet.author_id === userId) {
         continue
       }
       */
 
-      const toPostOn = chrono.parseDate(
-        tweetInfo.tweet.text,
-        new Date(tweetInfo.tweet.created_at),
-        { forwardDate: true }
-      )
+            const toPostOn = chrono.parseDate(tweetInfo.tweet.text, new Date(tweetInfo.tweet.created_at), {
+                forwardDate: true,
+            });
 
-      if (!toPostOn) {
-        continue
-      }
+            if (!toPostOn) {
+                continue;
+            }
 
-      const meta = { toPostOn }
-      await storage.insertIncomingTweet(tweetInfo, meta)
-      await storage.insertRepost(tweetInfo, meta)
-      await storage.insertConfirmation(tweetInfo, meta)
+            const meta = { toPostOn };
+            await storage.insertIncomingTweet(tweetInfo, meta);
+            await storage.insertRepost(tweetInfo, meta);
+            await storage.insertConfirmation(tweetInfo, meta);
+        }
+    } catch (e) {
+        console.warn('Error processing tweets', e);
+        error = e.stack;
+        mostRecentTweetId = lastRun?.mostRecentTweetId;
     }
-  } catch (e) {
-    console.warn("Error processing tweets", e)
-    error = e.stack
-    mostRecentTweetId = lastRun?.mostRecentTweetId
-  }
-  storage.insertJobData<ScrapeMentionsJobData>(JOB_NAME, {
-    mostRecentTweetId: mostRecentTweetId ?? lastRun?.mostRecentTweetId,
-    error
-  })
+    storage.insertJobData<ScrapeMentionsJobData>(JOB_NAME, {
+        mostRecentTweetId: mostRecentTweetId ?? lastRun?.mostRecentTweetId,
+        error,
+    });
 }
